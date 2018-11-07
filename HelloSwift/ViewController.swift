f74d9d2771cdc8ef2dfd8faa88f31afa1b9dd38e
supot10 <supot10@hotmail.com>
2018-11-07 11:27:41 +0700

    Start App

---
+++ HelloSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloSwift
+//
+//  Created by Macintosh HD on 11/7/2561 BE.
+//  Copyright © 2561 Supot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }   // main Function
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+} //mainclass
+
