62fc378d1809517e9fb65007d32a9d63c70469bf
supot10 <supot10@hotmail.com>
2018-11-07 13:39:51 +0700

    test

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ // Explicit
+    let nameString: String = "Doramon"
+    
+    
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+    
+    
+    @IBAction func changeButton(sender: AnyObject) {
+        
+        titleLabel.text = nameString
+        
+    } //
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
